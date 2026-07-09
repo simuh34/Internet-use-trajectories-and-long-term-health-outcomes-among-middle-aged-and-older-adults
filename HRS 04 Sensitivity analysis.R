65124d3c3df1f09c6a768da6a5ad30d482ab9aac
Simu Huang <70760926+simuh34@users.noreply.github.com>
2026-07-09 13:31:09 +0800

    Update HRS 04 Sensitivity analysis.R

--- HRS 04 Sensitivity analysis.R
+++ HRS 04 Sensitivity analysis.R
@@ -4,21 +4,21 @@ df_lag <- df[df_lag$death_year > 2012 | is.na(df_lag$death_year), ]
 model_lag <- coxph(Surv(survival_time, survival_status) ~ 
                            digital_usage + age + age_sq + gender + Race + 
                            residence_rural + H_education + marital_status + 
-                           equivalized_wealth, 
+                           equivalized_wealth + employment_status, 
                          data = df_lag, weights = iptw_weight)
 
 #2.complete information n = 9891
 model_complete <- coxph(Surv(survival_time, survival_status) ~ 
                                digital_usage + age + age_sq + gender + Race + 
                                residence_rural + H_education + marital_status + 
-                               equivalized_wealth, 
+                               equivalized_wealth + employment_status, 
                              data = df_complete, weights = iptw_weight)
 
 #3.sampling weight
 model_sample <- coxph(Surv(survival_time, survival_status) ~ 
                                     digital_usage + age + age_sq + gender + Race + 
                                     residence_rural + H_education + marital_status + 
-                                    equivalized_wealth, 
+                                    equivalized_wealth + employment_status, 
                                   data = df, weights = iptw_weight * WEIGHT)
 
 #4.iptw < 10
@@ -26,14 +26,14 @@ df_iptw5 <- filter(df, df$iptw_weight < 10)
 model_iptw5 <- coxph(Surv(survival_time, survival_status) ~ 
                                   digital_usage + age + age_sq + gender + Race + 
                                   residence_rural + H_education + marital_status + 
-                                  equivalized_wealth, 
+                                  equivalized_wealth + employment_status, 
                                 data = df_iptw5, weights = iptw_weight * WEIGHT)
 
 #5.ipcw
 model_sample <- coxph(Surv(survival_time, survival_status) ~ 
                                   digital_usage + age + age_sq + gender + Race + 
                                   residence_rural + H_education + marital_status + 
-                                  equivalized_wealth, 
+                                  equivalized_wealth + employment_status, 
                                 data = df, weights = iptw_weight * ipcw_cum_inw14)
 
 #6.E-value
